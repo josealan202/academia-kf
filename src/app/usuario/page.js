ca88ea2f4fae7c3170bcf5b231788552ac03e267
José Alan <alan.dias@academico@ifpb.edu.br>
2025-04-29 10:20:06 -0300

    Inserção do id - JSX e SQL

--- src/app/usuario/page.js
+++ src/app/usuario/page.js
@@ -1,6 +1,20 @@
-export default () => (
-    <>
-        <h1>Página do usuário</h1>
-        <p>...</p>
-    </>
-)
+import db from "@/lib/db"
+export default async () => {
+    const usuarios = await db.query("select * from usuario")
+ return (<>
+    <br></br>
+    <h1>Lista de usuários</h1>
+    <br></br>
+    <div>
+      {
+         usuarios.rows.map( 
+            u => (
+               <div>
+                  {u.nome} é {u.cor}.
+               </div>
+            ) 
+         )
+      }
+   </div>
+ </>);
+}
