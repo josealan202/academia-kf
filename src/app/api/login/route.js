1db6d42e76024ff7a87a69986ea7a63529b6a419
José Alan <alan.dias@academico@ifpb.edu.br>
2025-12-30 11:31:52 -0300

    Correção bcryptjs.

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import pool from "../../../lib/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export async function POST(request) {
   try {
